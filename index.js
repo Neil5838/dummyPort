7c999d92b7f450557e9fa1346454803c53212b22
Neil5838 <neilSE5838@gmail.com>
2025-02-17 20:34:10 +0545

    added hero-section

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const navToggle = document.getElementById("nav-toggle");
 
-const links = document.querySelector(".links");
+const linksContainer = document.querySelector(".links");
 navToggle.addEventListener("click", () => {
-  links.classList.toggle("show-links");
+  linksContainer.classList.toggle("show-links");
 });
+
+const links = document.querySelectorAll(".links li a");
+links.forEach((link) =>
+  link.addEventListener("click", () =>
+    linksContainer.classList.remove("show-links")
+  )
+);
