7f96fb2dcfa8d650274c294a454ac7c34df18e5b
Neil5838 <neilSE5838@gmail.com>
2025-02-18 10:28:34 +0545

    added otther pages

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const navToggle = document.getElementById("nav-toggle");
 const heroSection = document.getElementById("hero-section");
 
+const topBtn = document.querySelector(".goto-top");
+
 const linksContainer = document.querySelector(".links");
 navToggle.addEventListener("click", () => {
   linksContainer.classList.toggle("show-links");
@@ -15,4 +17,12 @@ links.forEach((link) =>
 
 heroSection.addEventListener("click", () => {
   linksContainer.classList.remove("show-links");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    topBtn.classList.add("show-top-btn");
+  } else {
+    topBtn.classList.remove("show-top-btn");
+  }
 });
